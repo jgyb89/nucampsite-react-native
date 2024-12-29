606406d577c80f2147aa902d5ac03be4c002f410
James Green <jgreen.gus@gmail.com>
2024-12-28 19:37:25 -0500

    Swipe and Delete Favorites

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,5 +1,7 @@
-import { useSelector } from 'react-redux';
-import { View, FlatList, Text } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { View, FlatList, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { SwipeRow } from 'react-native-swipe-list-view';
+import { toggleFavorite } from '../features/favorites/favoritesSlice';
 import { Avatar, ListItem } from 'react-native-elements';
 import Loading from '../components/LoadingComponent'
 import { baseUrl } from '../shared/baseUrl';
@@ -10,25 +12,60 @@ const FavoriteScrteen = ({ navigation }) => {
     );
     const favorites = useSelector((state) => state.favorites);
 
+    const dispatch = useDispatch();
+
     const renderFavoriteItem = ({ item: campsite }) => {
         return (
-            <ListItem
-                onPress={() => navigation.navigate('Directory', {
-                    screen: 'CampsiteInfo',
-                    params: { campsite }
-                })
-                }
-            >
+            <SwipeRow rightOpenValue={-100}>
+                <View style={styles.deleteView}>
+                    <TouchableOpacity 
+                        style={styles.deleteTouchable}
+                        onPress={() => dispatch(toggleFavorite(campsite.id))}
+                    >
+                        <Text style={styles.deleteText}>Delete</Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <ListItem
+                        onPress={() =>
+                            navigation.navigate('Directory', {
+                                screen: 'CampsiteInfo',
+                                params: { campsite }
+                            })
+                        }
+                    >
+                        <Avatar
+                            rounded
+                            source={{ uri: baseUrl + campsite.image }}
+                        />
+                        <ListItem.Content>
+                            <ListItem.Title>{campsite.name}</ListItem.Title>
+                            <ListItem.Subtitle>
+                                {campsite.description}
+                            </ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
+                </View>
+                <View>
+                    <ListItem
+                        onPress={() => navigation.navigate('Directory', {
+                            screen: 'CampsiteInfo',
+                            params: { campsite }
+                        })
+                        }
+                    >
 
 
-                <Avatar rounded source={{ uri: baseUrl + campsite.image }} />
-                <ListItem.Content>
-                    <ListItem.Title>{campsite.name}</ListItem.Title>
-                    <ListItem.Subtitle>
-                        {campsite.description}
-                    </ListItem.Subtitle>
-                </ListItem.Content>
-            </ListItem>
+                        <Avatar rounded source={{ uri: baseUrl + campsite.image }} />
+                        <ListItem.Content>
+                            <ListItem.Title>{campsite.name}</ListItem.Title>
+                            <ListItem.Subtitle>
+                                {campsite.description}
+                            </ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
+                </View>
+            </SwipeRow>
         )
     }
 
@@ -52,5 +89,30 @@ const FavoriteScrteen = ({ navigation }) => {
         />
     );
 };
+
+
+
+const styles = StyleSheet.create({
+    deleteView: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        flex: 1
+    },
+    deleteTouchable: {
+        backgroundColor: 'red',
+        height: '100%',
+        justifyContent: 'center'
+    },
+    deleteText: {
+        color: 'white',
+        fontWeight: '700',
+        textAlign: 'center',
+        fontSize: 16,
+        width: 100
+    }
+});
+
+
 
 export default FavoriteScrteen;
